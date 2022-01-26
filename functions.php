2866e7ec321297d95009557c78be2744eae48d3c
Рябцун Дмитрий Викторович <dryabtsun@badm.ua>
2022-01-26 14:30:47 +0200

    add function

--- functions.php
+++ functions.php
@@ -4,3 +4,20 @@ $page_content = renderTemplate('templates/index.php', [section=>$sections]);
 $layout_content = renderTemplate('templates/layot.php', ['content'=>$page_content, 'title'=>'Главная']);
 print($layout_content);
 
+function inclideTemplate($template, $array_data = []){
+    $template = 'templates' . $template;
+
+    if(!file_exists($template)){
+        return '';
+    }
+
+    extract($array_data);
+
+    ob_start();
+
+    require_once($template);
+
+    $html_template = ob_get_clean();
+
+    return $html_template;
+}
